9759131f0b65ff4a18a041e6405a7e6e038d557d
Azizi Walid <fullstackwebdeveloper@walidazizi.com>
2023-02-26 22:51:34 +0100

    List-school

---
+++ MvcSchool/Models/MvcSchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcSchool.Models
+{
+    public class MvcSchoolContext : DbContext
+    {
+
+        public MvcSchoolContext(DbContextOptions<MvcSchoolContext> options)
+            :base(options) 
+            {
+            
+            }
+        public DbSet<Student> Students { get; set; } = default!;
+        public DbSet<Course> Courses { get; set; } = default!;
+
+    }
+}
